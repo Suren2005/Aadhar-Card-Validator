03cea04d14a84303fbe56a92846ed3eba0ad643c
Suren2005 <130954822+Suren2005@users.noreply.github.com>
2023-08-03 10:44:12 +0530

    Add files via upload

---
+++ aadhar card.py
@@ -0,0 +1,11 @@
+num = input("Please enter a 12 digit aadhar number: ")
+if len(num) > 12 or len(num) < 12:
+
+    print("Aadhar number is not valid (Enter a 12 digit number)")
+elif num[0] == '0' or num[0] == '1':
+        print("Aadhar number is not valid (Aadhar card number cannot start with 0 or 1)")
+elif num.isalpha():
+     print("invalid Please Enter Correct Number")      
+else:
+    print("Aadhar number is valid")
+        
